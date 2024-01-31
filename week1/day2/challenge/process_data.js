44104f2e62b6448f5acc79ce84f2ed24bfcd4b28
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-01-31 08:38:59 +0000

    day2_fixes_v1

--- week1/day2/challenge/process_data.js
+++ week1/day2/challenge/process_data.js
@@ -1,10 +1,10 @@
 
 
-async function processUserData(data)
+async function processUserData({users})
 {
     try{
         // console.log("data = ", data)
-        let male_users = data["users"].filter(x => x.gender === "male");
+        let male_users = users.filter(x => x.gender === "male");
         let myman = male_users.map(({firstName, lastName, age})=> `Name: ${firstName} ${lastName}, Age: ${age}`);
         return {men : myman, men_arr: male_users};
     }
