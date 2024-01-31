244b62b3f0da2a815d380c93d0d17dba976c1362
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-01-31 09:11:40 +0000

    day2_fixes_v2.2

--- week1/day2/challenge/fetch_data.js
+++ week1/day2/challenge/fetch_data.js
@@ -7,16 +7,16 @@ async function fetchUserData()
         let res = await fetch("https://dummyjson.com/users");
         let data = await res.json();
 
-        console.log("==========");
-        console.log("data['users'] = ", data["users"].slice(0,7));
-        console.log("===========");
+        // console.log("==========");
+        // console.log("data['users'] = ", data["users"].slice(0,7));
+        // console.log("===========");
 
-        let {women_arr_str, women_arr} = await processUserData(data);
-        let sum_ages = summarizeAge(women_arr);
+        let {men_arr_str, men_arr} = await processUserData(data);
+        let sum_ages = summarizeAge(men_arr);
         console.log("Processed Users: ");
-        for(let i = 0; i < women_arr_str.length; i++)
+        for(let i = 0; i < men_arr_str.length; i++)
         {
-            console.log(`- ${women_arr_str[i]}`);
+            console.log(`- ${men_arr_str[i]}`);
         }
         console.log(`Total Age of Active Users: ${sum_ages}`);
     }
--- week1/day2/challenge/process_data.js
+++ week1/day2/challenge/process_data.js
@@ -1,18 +1,19 @@
-
-
-async function processUserData({users})
-{
-    try{
-        // console.log("data = ", data)
-        let women_users = users.filter(x => x.gender !== "male");
-        let formatted_women = women_users.map(({firstName, lastName, age})=> `Name: ${firstName} ${lastName}, Age: ${age}`);
-        return {women_arr_str: formatted_women, women_arr: women_users};
-    }
-    catch(err)
-    {
-        console.log(err);
-        throw err;
-    }
+async function processUserData({ users }) {
+  try {
+    // console.log("data = ", data)
+    let men_users = users.filter((x) => x.gender === "male");
+    let formatted_men = men_users.map(
+      ({ firstName, lastName, age }) =>
+        `Name: ${firstName} ${lastName}, Age: ${age}`
+    );
+    return {
+      men_arr_str: formatted_men,
+      men_arr: men_users,
+    };
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 }
 
 module.exports = processUserData;
