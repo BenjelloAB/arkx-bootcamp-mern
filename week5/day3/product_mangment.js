af81f14b511fbf005b68901304d493e06fe576b0
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2024-03-01 03:54:25 +0000

    day3 week5 v1.0

---
+++ week5/day3/product_mangment.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/mydb2";
+const products = require("./products");
+
+async function main() {
+  try {
+    await mongoose.connect(url);
+    var client = mongoose.connection.getClient();
+    console.log("Connected to the MongoDB Server");
+
+    const ProductSchema = new mongoose.Schema(
+      {
+        name: { type: String, required: true },
+        price: {
+          type: Number,
+          required: true,
+          validate: {
+            validator: function (value) {
+              return value > 0;
+            },
+            message: (props) => `${props.value} is not a positive number`,
+          },
+        },
+        description: { type: String },
+        inStock: { type: Boolean, default: true },
+      },
+      {
+        timestamps: true,
+      }
+    );
+
+    const ProductModel = mongoose.model("Product", ProductSchema);
+    //!inserting some dummy products
+    // await ProductModel.insertMany(products);
+
+
+    //! sorting desc:
+    const product_sorted = await ProductModel.find().sort({ price: -1 });
+    console.log(product_sorted);
+
+    //! pagination :
+    const products_paginated = await ProductModel.find().limit(5);
+    console.log(products_paginated);
+
+    //! pagination with variables
+    const pageSize = 2;
+    const pageNumber = 3;
+    const prods = await ProductModel.find()
+      .sort({ price: -1 })
+      .skip(pageNumber * pageSize)
+      .limit(pageSize);
+    console.log(prods);
+
+    //!Aggreation Count porducts in stock
+    const c = await ProductModel.aggregate([
+      {
+        $group: {
+          _id: "",
+          count: { $sum: "$price" },
+        },
+      },
+    ]);
+    console.log(c[0].count);
+
+    //! Sort by name asc:
+    const prods_ = await ProductModel.aggregate([
+      {
+        $sort: { name: 1 },
+      },
+    ]);
+    console.log(prods_)
+
+    client.close()
+  } catch (err) {
+    console.log(err.message);
+    client.close()
+  }
+}
+
+main();
---
+++ week5/day3/products.js
@@ -0,0 +1,125 @@
+const products = [
+    {
+      name: 'Laptop',
+      price: 1200,
+      description: 'High-performance laptop with powerful specs.',
+      inStock: true,
+    },
+    {
+      name: 'Smartphone',
+      price: 800,
+      description: 'Latest smartphone with advanced features.',
+      inStock: true,
+    },
+    {
+      name: 'Headphones',
+      price: 150,
+      description: 'Over-ear headphones with noise-cancelling technology.',
+      inStock: true,
+    },
+    {
+      name: 'Smartwatch',
+      price: 250,
+      description: 'Fitness tracker and smartwatch with health monitoring.',
+      inStock: false,
+    },
+    {
+      name: 'Camera',
+      price: 600,
+      description: 'Digital camera with high-resolution imaging.',
+      inStock: true,
+    },
+    {
+      name: 'Gaming Console',
+      price: 400,
+      description: 'Next-gen gaming console for immersive gaming experiences.',
+      inStock: true,
+    },
+    {
+      name: 'Bluetooth Speaker',
+      price: 80,
+      description: 'Portable Bluetooth speaker with crisp sound.',
+      inStock: true,
+    },
+    {
+      name: 'Tablet',
+      price: 300,
+      description: 'Slim and lightweight tablet for on-the-go productivity.',
+      inStock: true,
+    },
+    {
+      name: 'Coffee Maker',
+      price: 50,
+      description: 'Automatic coffee maker for brewing your favorite coffee.',
+      inStock: true,
+    },
+    {
+      name: 'Fitness Tracker',
+      price: 100,
+      description: 'Wearable fitness tracker with heart rate monitoring.',
+      inStock: false,
+    },
+    {
+      name: 'External Hard Drive',
+      price: 120,
+      description: 'Large-capacity external hard drive for data storage.',
+      inStock: true,
+    },
+    {
+      name: 'Wireless Mouse',
+      price: 30,
+      description: 'Ergonomic wireless mouse for comfortable computing.',
+      inStock: true,
+    },
+    {
+      name: 'Portable Charger',
+      price: 20,
+      description: 'Compact portable charger for on-the-go device charging.',
+      inStock: true,
+    },
+    {
+      name: 'Smart Bulbs',
+      price: 15,
+      description: 'Set of smart bulbs for customizable lighting at home.',
+      inStock: true,
+    },
+    {
+      name: 'Backpack',
+      price: 40,
+      description: 'Durable backpack with multiple compartments for storage.',
+      inStock: true,
+    },
+    {
+      name: 'Wireless Earbuds',
+      price: 120,
+      description: 'True wireless earbuds for immersive audio experiences.',
+      inStock: false,
+    },
+    {
+      name: 'Graphic Tablet',
+      price: 200,
+      description: 'Digital graphic tablet for artists and designers.',
+      inStock: true,
+    },
+    {
+      name: 'Desk Chair',
+      price: 150,
+      description: 'Comfortable desk chair with adjustable features.',
+      inStock: true,
+    },
+    {
+      name: 'Air Purifier',
+      price: 80,
+      description: 'HEPA air purifier for cleaner and fresher indoor air.',
+      inStock: true,
+    },
+    {
+      name: 'Electric Toothbrush',
+      price: 40,
+      description: 'Electric toothbrush for effective dental care.',
+      inStock: true,
+    },
+  ];
+
+
+  module.exports = products;
